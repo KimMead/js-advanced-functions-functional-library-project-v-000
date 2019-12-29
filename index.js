12810b7eda693cf5a00816b98a242a553ff5b042
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-12-29 21:56:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let arr;
+      if(collection instanceof Array) {
+        arr = collection 
+      } else {
+        arr = Object.values(collection)
+      }
+      arr.forEach(element => {
+        callback(element)
+      });
+      return collection
     },
 
     map: function() {
